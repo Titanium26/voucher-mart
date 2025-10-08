70a5f994981a4cbb534d678024fe7a8bfccd16e0
Amir Mosadeghzad <amirmosadeghzad@gmail.com>
2025-10-08 06:36:11 -0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} scale-page-80 antialiased`}
       >
         {children}
       </body>
